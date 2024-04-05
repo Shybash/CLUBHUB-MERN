1b78138e8bfd4fd88526359a05e97c5015b33885
Shybash <shybashshaik@gmail.com>
2024-04-05 12:57:25 +0530

    updated_frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 // Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.scss';
+import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar fixed-top navbar-expand-lg">
